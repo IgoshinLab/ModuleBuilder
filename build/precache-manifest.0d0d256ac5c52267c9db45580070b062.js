4a856c723d1c5479d51651e9d31b14a01fdc31bb
JiangguoZhang <zhang_jiangguo@outlook.com>
2020-06-25 23:19:38 -0500

    heroku

---
+++ build/precache-manifest.0d0d256ac5c52267c9db45580070b062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9960564b90e419f9a726de622178254c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11e06e08fcccd680c56d",
+    "url": "/static/css/main.1acbe7e5.chunk.css"
+  },
+  {
+    "revision": "c7481dd390f05d9cc211",
+    "url": "/static/js/2.d6b1589a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d6b1589a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11e06e08fcccd680c56d",
+    "url": "/static/js/main.4ebb4d79.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
